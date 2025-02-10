6cc1695d9ae184c5053b125258bac0f378fed179
AJaxRUN <nujrachembai@gmail.com>
2025-02-09 21:08:18 -0500

    feat: test rn1 node creation

--- ros2_ws/entrypoint.sh
+++ ros2_ws/entrypoint.sh
@@ -5,11 +5,11 @@ source /opt/ros/humble/setup.bash
 source /ros2_ws/install/setup.bash
 
 # Add ROS2 workspace to PYTHONPATH
-# export PYTHONPATH="/ros2_ws/install/lib/python3.10/site-packages:$PYTHONPATH"
+export PYTHONPATH="/ros2_ws/install/lib/python3.10/site-packages:$PYTHONPATH"
 
 # Start FastAPI in the background
 # uvicorn src.rest_api.main:app --host 0.0.0.0 --port 8000 &
 
 # Start ROS2 nodes
-ros2 run rn1 client_node
+# ros2 run mynode talker
 tail -f /dev/null
